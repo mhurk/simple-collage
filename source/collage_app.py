a9489ff7ee6800eb2e5869bdb7a06148f24101b3
mhurk <149044252+mhurk@users.noreply.github.com>
2024-11-26 20:47:53 +0100

    bugfix (issue#3)

--- source/collage_app.py
+++ source/collage_app.py
@@ -29,24 +29,6 @@ def rotate_image_based_on_exif(img):
         pass
     return img
 
-def rotate_image_based_on_exif(img):
-    try:
-        for orientation in ExifTags.TAGS.keys():
-            if ExifTags.TAGS[orientation] == 'Orientation':
-                break
-        exif = img._getexif()
-        if exif is not None:
-            orientation_value = exif.get(orientation)
-            if orientation_value == 3:
-                img = img.rotate(180, expand=True)
-            elif orientation_value == 6:
-                img = img.rotate(270, expand=True)
-            elif orientation_value == 8:
-                img = img.rotate(90, expand=True)
-    except (AttributeError, KeyError, IndexError):
-        pass
-    return img
-
 def resize_image_proportionally(img, target_height):
     aspect_ratio = img.width / img.height
     new_width = int(target_height * aspect_ratio)
